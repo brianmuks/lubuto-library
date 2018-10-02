0962fa2d86a0d027101c35d0f0977f7895c1a56f
Olivier JM <manolivier93@gmail.com>
2018-10-02 22:11:18 +0200

    cleaned the home lesson page

--- imports/ui/Components/LessonView.js
+++ imports/ui/Components/LessonView.js
@@ -6,15 +6,15 @@ export default class LessonView extends Component {
       <Fragment>
         <div className="row">
           <div
-            className="col s12 m4 l3 sidebar blue-grey lighten-2"
+            className="col s12 m4 l3 sidebar"
           >
-            <ul>
-                <li>Page 1</li>
-                <li>Page 2</li>
-                <li>Page 3</li>
-            </ul>
+             <div className="collection">
+                <a href="/1" className="collection-item">Page 1</a>
+                <a href="/2" className="collection-item active">Page 2</a>
+                <a href="/3" className="collection-item">Page 3</a>
+                <a href="/4" className="collection-item">Page 4</a>
+            </div>
           </div>
-
           <div className="col s12 m8 l9 main teal lighten-1">main</div>
         </div>
 
