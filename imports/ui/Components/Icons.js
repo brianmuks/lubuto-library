315c38820d3db40a60f8ea85e4ccf0825a553625
Olivier JM <manolivier93@gmail.com>
2018-10-31 11:30:03 +0200

    added draggable icons

---
+++ imports/ui/Components/Icons.js
@@ -0,0 +1,9 @@
+export default [
+   { name: 'home', size: 'large'},
+   { name: 'arrow_right_alt', size: 'large'},
+   { name: 'check_circle', size: 'large'},
+   { name: 'done', size: 'large'},
+   { name: 'label', size: 'large'},
+   { name: 'perm_identity', size: 'large'},
+   { name: 'trending_flat', size: 'large'}
+]
