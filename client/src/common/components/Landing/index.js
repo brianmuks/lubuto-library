110d3d34f4d129cb70b51a7d2dc33d5d42df9d8e
Olivier JM <manolivier93@gmail.com>
2018-11-06 15:58:49 +0200

    cleaned the landing page

--- client/src/common/components/Landing/index.js
+++ client/src/common/components/Landing/index.js
@@ -1,30 +1,31 @@
-import React, { Fragment } from "react"
-import './landing.css'
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+import "./landing.css";
 
 const Landing = () => (
   <Fragment>
-      <div className='container-fluid'>
-    <nav className="light-blue lighten-1 container-fluid" role="navigation">
-      <div className="nav-wrapper container">
-        <a id="logo-container" href="#" className="brand-logo">
-            Library
-        </a>
-        <ul className="right hide-on-med-and-down">
-          <li>
-            <a href="#">Dashboard</a>
-          </li>
-        </ul>
+    <div className="container-fluid">
+      <nav className="light-blue lighten-1 container-fluid" role="navigation">
+        <div className="nav-wrapper">
+          <a id="logo-container" href="#" className="brand-logo">
+            Lubuto Library
+          </a>
+          <ul className="right hide-on-med-and-down">
+            <li>
+              <a href="#">Dashboard</a>
+            </li>
+          </ul>
 
-        <ul id="nav-mobile" className="sidenav">
-          <li>
-            <a href="#">Navbar Link</a>
-          </li>
-        </ul>
-        <a href="#" data-target="nav-mobile" className="sidenav-trigger">
-          <i className="material-icons">menu</i>
-        </a>
-      </div>
-    </nav>
+          <ul id="nav-mobile" className="sidenav">
+            <li>
+              <a href="#">Navbar Link</a>
+            </li>
+          </ul>
+          <a href="#" data-target="nav-mobile" className="sidenav-trigger">
+            <i className="material-icons">menu</i>
+          </a>
+        </div>
+      </nav>
     </div>
     <div className="section no-pad-bot" id="index-banner">
       <div className="container">
@@ -33,17 +34,15 @@ const Landing = () => (
         <h1 className="header center orange-text">Modern Lessons</h1>
         <div className="row center">
           <h5 className="header col s12 light">
-          Libraries transforming Africa's next generation
+            Libraries transforming Africa's next generation
           </h5>
         </div>
         <div className="row center">
-          <a
-            href="http://materializecss.com/getting-started.html"
-            id="download-button"
-            className="btn-large waves-effect waves-light orange"
-          >
-            Get Started
-          </a>
+           <Link to="/dashboard/create-lesson">
+              <button  className="btn-large waves-effect waves-light blue" >
+              Create Lessons
+              </button>
+          </Link>
         </div>
         <br />
         <br />
@@ -58,13 +57,10 @@ const Landing = () => (
               <h2 className="center light-blue-text">
                 <i className="material-icons">flash_on</i>
               </h2>
-              <h5 className="center">Speeds up development</h5>
+              <h5 className="center">Speeds up Learning</h5>
 
               <p className="light center">
-                We did most of the heavy lifting for you to provide a default
-                stylings that incorporate our custom components. Additionally,
-                we refined animations and transitions to provide a smoother
-                experience for developers.
+                Learners will find this an easy way to learn
               </p>
             </div>
           </div>
@@ -94,7 +90,7 @@ const Landing = () => (
               <h5 className="center">Easy to work with</h5>
 
               <p className="light center">
-                We have provided detailed documentation as well as specific code
+                We will provided detailed documentation as well as specific code
                 examples to help new users get started. We are also always open
                 to feedback and can answer any questions a user may have about
                 Materialize.
@@ -113,7 +109,7 @@ const Landing = () => (
           <div className="col l6 s12">
             <h5 className="white-text">Company Bio</h5>
             <p className="grey-text text-lighten-4">
-              Doing the best to provide with the best lessons 
+              Doing the best to provide with the best lessons
             </p>
           </div>
           <div className="col l3 s12">
@@ -138,19 +134,16 @@ const Landing = () => (
                 <a className="white-text" href="#!">
                   Create Lessons
                 </a>
-                </li>
+              </li>
             </ul>
           </div>
         </div>
       </div>
       <div className="footer-copyright">
-        <div className="container">
-          Made by{" "}
-            BrianMuks Co 
-        </div>
+        <div className="container">Made by BrianMuks Co</div>
       </div>
     </footer>
   </Fragment>
-)
+);
 
-export default Landing
+export default Landing;
