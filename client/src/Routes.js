7cce782502776e15f9bbee326eaff97f191f939d
Olivier JM <manolivier93@gmail.com>
2018-11-10 22:09:44 +0200

    added a notfound page

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import IconEditor from "./dashboard/tools/iconEditor";
 import CreateLesson from "./dashboard/d-lesson/CreateLesson";
 import Landing from "./common/components/Landing";
+import NotFound from './common/components/NotFound'
 
 import Admin from "./Routes/Admin";
 import User from "./Routes/User";
@@ -21,6 +22,7 @@ const Routes = () => (
       />
       {/* <Route path="/dashboard/create-lesson" component={CreateLesson} /> */}
       <User exact role={"user"} path="/" component={Landing} />
+      <Route component={NotFound} />
     </Switch>
   </Router>
 );
