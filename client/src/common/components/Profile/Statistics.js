f90e37df530f0c8510beee4091b99d40c76d05c0
Olivier JM <manolivier93@gmail.com>
2018-12-02 18:41:48 +0200

    added a statistics file

---
+++ client/src/common/components/Profile/Statistics.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+function Statistics() {
+  return (
+    <div className="container">
+      <h4>Users </h4>
+      <table className="highlight">
+        <thead>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Sex</th>
+          <th>Center</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Olivier JM</td>
+            <td>25 </td>
+            <td>Male </td>
+            <td>Lusaka </td>
+          </tr>
+          <tr>
+            <td>Olivier JM</td>
+            <td>25 </td>
+            <td>Male </td>
+            <td>Lusaka </td>
+          </tr>
+          <tr>
+            <td>Olivier JM</td>
+            <td>25 </td>
+            <td>Male </td>
+            <td>Lusaka </td>
+          </tr>
+          <tr>
+            <td>Olivier JM</td>
+            <td>25 </td>
+            <td>Male </td>
+            <td>Lusaka </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Statistics;
--- client/src/common/components/Profile/UserProfile.js
+++ client/src/common/components/Profile/UserProfile.js
@@ -1,29 +1,27 @@
-import React from 'react'
+import React from "react";
 
-function UserProfile(){
-
-return (
-    <div className='container'>
-        <h4>Olivier JM </h4>
-    <table className='highlight'>
+function UserProfile() {
+  return (
+    <div className="container">
+      <h4>Olivier JM </h4>
+      <table className="highlight">
         <thead>
-            <th>Name</th>
-            <th>Age</th>
-            <th>Sex</th>
-            <th>Center</th>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Sex</th>
+          <th>Center</th>
         </thead>
         <tbody>
-            <tr>
-                <td>Olivier JM</td>
-                <td>25 </td>
-                <td>Male </td>
-                <td>Lusaka </td>
-                </tr>
+          <tr>
+            <td>Olivier JM</td>
+            <td>25 </td>
+            <td>Male </td>
+            <td>Lusaka </td>
+          </tr>
         </tbody>
-    </table>
-</div>
-)
+      </table>
+    </div>
+  );
 }
 
-
-export default UserProfile
+export default UserProfile;
