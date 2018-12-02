838494cefaf7f12a533438b51f4cb16c6347ab15
Olivier JM <manolivier93@gmail.com>
2018-12-02 18:32:02 +0200

    added a profile page

---
+++ client/src/common/components/Profile/UserProfile.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function UserProfile(){
+
+return (
+    <table>
+        <thead>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Sex</th>
+            <th>Center</th>
+        </thead>
+        <tbody>
+            <tr>Olivier JM</tr>
+            <tr>25</tr>
+            <tr>Male</tr>
+            <tr>Lusaka</tr>
+        </tbody>
+    </table>
+)
+}
+
+
+export default UserProfile
