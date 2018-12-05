6fde79ea6970d21ea1c74242b391f1b17cec7583
Olivier JM <manolivier93@gmail.com>
2018-12-05 17:46:33 +0200

    added a modal to edit users

--- client/src/common/components/Profile/Statistics.js
+++ client/src/common/components/Profile/Statistics.js
@@ -1,13 +1,47 @@
-import React from "react";
-import { Meteor } from "meteor/meteor";
-import { withTracker } from "meteor/react-meteor-data";
-import User from "./User";
+import React, { useState } from "react"
+import { Meteor } from "meteor/meteor"
+import { withTracker } from "meteor/react-meteor-data"
+import User from "./User"
+// import EditModal from '../Utils'
+import ReactModal from 'react-modal'
 
 const getAllUsers = users => users.length && <User users={users} />;
-let index = 0;
+let index = 0
 
 function Statistics({ users }) {
+  const [isOpen, setModal ] = useState(false)
+
+  function checkModal(){
+    setModal(!isOpen)
+  }
+  function saveChanges(){
+    console.log('Thats me ')
+  }
+  function editUser(){
+    setModal(true)
+  }
   return (
+    <>
+         <ReactModal 
+              isOpen={isOpen}
+              contentLabel="Minimal Modal Example"
+              ariaHideApp={false}
+              style={{
+                overlay: {
+                  backgroundColor: 'gray'
+                },
+                content: {
+                  color: 'lightsteelblue',
+                  top: '20%',
+                  left: '20%',
+                  bottom: '20%',
+                  right: '20%'
+                },
+                
+              }}
+            >
+          <button onClick={checkModal}>Close Modal</button>
+        </ReactModal>
     <div className="container">
       <h4>Users </h4>
       <table className="highlight">
@@ -18,22 +52,24 @@ function Statistics({ users }) {
             <th>Age</th>
             <th>Sex</th>
             <th>Center</th>
+            <th>Edit</th>
           </tr>
         </thead>
         <tbody>
           {users.map(user => (
-            <User key={user._id} user={user} count={users.length} i={index++} />
+            <User key={user._id} user={user} count={users.length} i={index++} editUser={editUser} />
           ))}
         </tbody>
       </table>
     </div>
-  );
+    </>
+  )
 }
 
 export default withTracker(() => {
   Meteor.subscribe("users");
   Meteor.subscribe("userStats");
   return {
-    users: Meteor.users.find({}).fetch()
-  };
-})(Statistics);
+    users: Meteor.users.find({}, { sort: { createdAt: -1 } }).fetch()
+  }
+})(Statistics)
