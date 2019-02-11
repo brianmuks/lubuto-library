3de4731b9c4c0eff817b0934159bdd96f9a74dfb
Olivier JM <manolivier93@gmail.com>
2019-02-11 11:08:26 +0200

    added a separate lesson stat

--- client/src/common/components/Profile/LessonStats.js
+++ client/src/common/components/Profile/LessonStats.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 import { Meteor } from "meteor/meteor";
-import { withRouter, Redirect, Link } from "react-router-dom";
+import { withRouter, Redirect } from "react-router-dom";
 import { withTracker } from "meteor/react-meteor-data";
 import {
   BarChart,
@@ -18,17 +18,15 @@ import { useLogout } from "../Accounts/accountsUtils";
 import { NavBar } from "../Landing";
 
 const data = [
-  {name: 'Lesson 1', correct_answer: 40, tries: 2, amt: 2400},
-  {name: 'Lesson 2', correct_answer: 30, tries: 1, amt: 2210},
-  {name: 'Lesson 3', correct_answer: 20, tries: 9, amt: 2290},
-  {name: 'Lesson 4', correct_answer: 27, tries: 3, amt: 2000},
-  {name: 'Lesson 5', correct_answer: 18, tries: 4, amt: 2181},
-  {name: 'Lesson 6', correct_answer: 23, tries: 3, amt: 2500},
-  {name: 'Lesson 7', correct_answer: 34, tries: 4, amt: 2100},
+  {name: 'Page 1', time_taken: 40, tries: 2, amt: 2400},
+  {name: 'Page 2', time_taken: 30, tries: 1, amt: 2210},
+  {name: 'Page 3', time_taken: 20, tries: 9, amt: 2290},
+  {name: 'Page 4', time_taken: 27, tries: 3, amt: 2000},
+  {name: 'Page 5', time_taken: 18, tries: 4, amt: 2181},
 ];
 
 
-function UserProfile({ user, stats, history }) {
+function LessonStats({ user, stats, history }) {
   const { isLoggedOut, logOutUser } = useLogout();
   if (isLoggedOut) {
     return <Redirect to="/login" />;
@@ -37,18 +35,19 @@ function UserProfile({ user, stats, history }) {
     <Fragment>
       <NavBar logOutUser={logOutUser} color={"light-blue"} />
       <div className="container">
-        <h4>{user && user.profile.name} </h4>
+        <h4>Lesson 1</h4>
         <table className="highlight">
           <thead>
             <tr>
-              <th>Name</th>
-              <th>Age</th>
-              <th>Sex</th>
-              <th>Center</th>
+              <th>Lesson</th>
+              <th>Pages</th>
             </tr>
           </thead>
           <tbody>
-            <User user={user} />
+            <tr>
+                <td>Lesson 1</td>
+                <td>5</td>
+            </tr>
           </tbody>
         </table>
         {/* 
@@ -70,7 +69,7 @@ function UserProfile({ user, stats, history }) {
             <YAxis />
             <Tooltip />
             <Legend />
-            <Bar onClick={() => history.push('/stats')} dataKey="correct_answer" fill="#8884d8" />
+            <Bar dataKey="time_taken" fill="#8884d8" />
             <Bar dataKey="tries" fill="#82ca9d" />
           </BarChart>
         </div>
@@ -80,7 +79,7 @@ function UserProfile({ user, stats, history }) {
 }
 
 // avoiding chanined wraps of two higher components
-const RouterProfile = withRouter(UserProfile);
+const RouterProfile = withRouter(LessonStats);
 
 export default withTracker(props => {
   Meteor.subscribe("user", props.match.params.id);
