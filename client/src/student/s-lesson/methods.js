b17066857602ee6ad5980f9549b1125e69ed9bf9
Brian Mukuka <brianmuks@gmail.com>
2021-08-20 14:25:43 +0200

    fixed bug > css className not remove on failed attempt . Fixed this issue with onDragEnd on multiple methods

--- client/src/student/s-lesson/methods.js
+++ client/src/student/s-lesson/methods.js
@@ -52,7 +52,7 @@ export const onDrop = (ev, ans, draggedQuestion, lessonId) => {
         playAudio(NO_SOUND);
         //{ MinimongoError: Key question.[object Object] must not contain '.'
         recordAttempt({ questionIndex, lessonId, passed: false });
-//  onAfterDrop({ isAdd: false });
+ onAfterDrop({ isAdd: false });
 
         // TODO:placyAudio  
         return;
@@ -80,7 +80,7 @@ export const onDrop = (ev, ans, draggedQuestion, lessonId) => {
     }
     ev.target.appendChild(draggedItem);
 
-//  onAfterDrop({isAdd:false});
+ onAfterDrop({isAdd:false});
 
 
      
